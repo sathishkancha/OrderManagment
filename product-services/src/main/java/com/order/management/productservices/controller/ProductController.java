16f363addcc238924338462ac8462724d40799cd
sathishkancha <sathishkumarkancha@gmail.com>
2021-12-12 21:14:11 +0530

    change URL for Product

--- product-services/src/main/java/com/order/management/productservices/controller/ProductController.java
+++ product-services/src/main/java/com/order/management/productservices/controller/ProductController.java
@@ -3,22 +3,20 @@ package com.order.management.productservices.controller;
 import com.order.management.productservices.entity.Product;
 import com.order.management.productservices.service.ProductserviceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api/order-management/product")
 public class ProductController {
     @Autowired
     private ProductserviceImpl productService;
-    @PostMapping("/post")
+    @PostMapping("")
     public Product postProduct(@RequestBody Product product){
         return productService.postProduct(product);
     }
-@GetMapping("/get")
+    @GetMapping("")
     public List<Product> getProduct(){
         return productService.getProduct();
 }
