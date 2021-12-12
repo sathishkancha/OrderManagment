e90395708e6e622f2bc68940cd1f35a14986e708
sathishkancha <sathishkumarkancha@gmail.com>
2021-12-12 23:05:44 +0530

    SearchProducts By Category

--- product-services/src/main/java/com/order/management/productservices/service/ProductCategoryService.java
+++ product-services/src/main/java/com/order/management/productservices/service/ProductCategoryService.java
@@ -1,6 +1,8 @@
 package com.order.management.productservices.service;
 
+import com.order.management.productservices.entity.AllBooksWithCategoryResponce;
 import com.order.management.productservices.entity.ProductCategory;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -8,4 +10,5 @@ public interface ProductCategoryService {
     public ProductCategory postProductCategory(ProductCategory productCategory);
     List<ProductCategory> getProductCategory();
     public ProductCategory fetchProductCategoryByName(String productCategoryName);
+    public AllBooksWithCategoryResponce fetchProductByCategory(String pcname);
 }
--- product-services/src/main/java/com/order/management/productservices/service/ProductCategoryServiceImpl.java
+++ product-services/src/main/java/com/order/management/productservices/service/ProductCategoryServiceImpl.java
@@ -1,7 +1,10 @@
 package com.order.management.productservices.service;
 
+import com.order.management.productservices.entity.AllBooksWithCategoryResponce;
+import com.order.management.productservices.entity.Product;
 import com.order.management.productservices.entity.ProductCategory;
 import com.order.management.productservices.repository.ProductCategoryRepository;
+import com.order.management.productservices.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,6 +15,10 @@ public class ProductCategoryServiceImpl implements ProductCategoryService {
 
     @Autowired
     ProductCategoryRepository productCategoryRepository;
+
+    @Autowired
+    ProductRepository productRepository;
+
     public ProductCategory postProductCategory(ProductCategory category) {
         return  productCategoryRepository.save(category);
     }
@@ -21,5 +28,15 @@ public class ProductCategoryServiceImpl implements ProductCategoryService {
 
     public ProductCategory fetchProductCategoryByName(String productCategoryName) {
         return productCategoryRepository.findByproductCategoryNameIgnoreCase(productCategoryName);
+    }
+
+    @Override
+    public AllBooksWithCategoryResponce fetchProductByCategory(String pcname) {
+        AllBooksWithCategoryResponce allBooksWithCategoryResponce=new AllBooksWithCategoryResponce();
+        ProductCategory pc = productCategoryRepository.findByproductCategoryNameIgnoreCase(pcname);
+        allBooksWithCategoryResponce.setCategory(pc);
+        List<Product> lp = productRepository.findByCategoryIgnoreCase(pcname);
+        allBooksWithCategoryResponce.setProduct(lp);
+        return allBooksWithCategoryResponce;
     }
 }
